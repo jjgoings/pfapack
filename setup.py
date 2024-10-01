716afbfa06cbec8e41cac69ec2ea8b737e0c4217
Joshua Goings <3915169+jjgoings@users.noreply.github.com>
2024-10-01 09:37:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class CustomCleanCommand(Command):
 setup(
     name="pfapack-batched",  # Changed from "pfapack" to "pfapack-batched"
     python_requires=">=3.9",
-    version="0.0.1",
+    version="0.0.2",
     cmdclass={
         'build_ext': CustomBuildExtCommand,
         'build_py': CustomBuildPyCommand,
