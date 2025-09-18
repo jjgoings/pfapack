8ec472e327664673d2c68bf550202b07827eaf8b
jjgoings <3915169+jjgoings@users.noreply.github.com>
2025-09-18 10:23:56 -0700

    fortran layout

--- external/c_interface/skpfa_batched.c
+++ external/c_interface/skpfa_batched.c
@@ -7,6 +7,7 @@
 #include <complex.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -252,6 +253,94 @@ int skpfa_batched_4d_z(int outer_batch_size, int inner_batch_size, int N,
     return 0;
 }
 
+int skpfa_batched_4d_d_f(int outer_batch_size, int inner_batch_size, int N,
+                         double *A_batch, double *PFAFF_batch,
+                         const char *UPLO, const char *MTHD)
+{
+    char uplo = toupper(UPLO[0]);
+    char mthd = toupper(MTHD[0]);
+
+    if (N < 0) return -1;
+    if (A_batch == NULL) return -2;
+    if (PFAFF_batch == NULL) return -3;
+    if (uplo != 'U' && uplo != 'L') return -4;
+    if (mthd != 'P' && mthd != 'H') return -5;
+
+    if (N > 0) {
+        double *A_work = (double *)malloc((size_t)N * N * sizeof(double));
+        if (!A_work) return -100;
+
+        const int total_matrices = outer_batch_size * inner_batch_size;
+        const size_t matrix_elems = (size_t)N * N;
+        const size_t matrix_bytes = matrix_elems * sizeof(double);
+
+        for (int idx = 0; idx < total_matrices; idx++) {
+            double *A = A_batch + idx * matrix_elems;
+            memcpy(A_work, A, matrix_bytes);
+
+            int ret = skpfa_d(N, A_work, &PFAFF_batch[idx], UPLO, MTHD);
+            if (ret != 0) {
+                free(A_work);
+                return ret;
+            }
+        }
+
+        free(A_work);
+    }
+    else {
+        const int total_matrices = outer_batch_size * inner_batch_size;
+        for (int i = 0; i < total_matrices; i++) {
+            PFAFF_batch[i] = 1.0;
+        }
+    }
+
+    return 0;
+}
+
+int skpfa_batched_4d_z_f(int outer_batch_size, int inner_batch_size, int N,
+                         doublecmplx *A_batch, doublecmplx *PFAFF_batch,
+                         const char *UPLO, const char *MTHD)
+{
+    char uplo = toupper(UPLO[0]);
+    char mthd = toupper(MTHD[0]);
+
+    if (N < 0) return -1;
+    if (A_batch == NULL) return -2;
+    if (PFAFF_batch == NULL) return -3;
+    if (uplo != 'U' && uplo != 'L') return -4;
+    if (mthd != 'P' && mthd != 'H') return -5;
+
+    if (N > 0) {
+        doublecmplx *A_work = (doublecmplx *)malloc((size_t)N * N * sizeof(doublecmplx));
+        if (!A_work) return -100;
+
+        const int total_matrices = outer_batch_size * inner_batch_size;
+        const size_t matrix_elems = (size_t)N * N;
+        const size_t matrix_bytes = matrix_elems * sizeof(doublecmplx);
+
+        for (int idx = 0; idx < total_matrices; idx++) {
+            doublecmplx *A = A_batch + idx * matrix_elems;
+            memcpy(A_work, A, matrix_bytes);
+
+            int ret = skpfa_z(N, A_work, &PFAFF_batch[idx], UPLO, MTHD);
+            if (ret != 0) {
+                free(A_work);
+                return ret;
+            }
+        }
+
+        free(A_work);
+    }
+    else {
+        const int total_matrices = outer_batch_size * inner_batch_size;
+        for (int i = 0; i < total_matrices; i++) {
+            PFAFF_batch[i] = doublecmplx_one;
+        }
+    }
+
+    return 0;
+}
+
 int skpfa_batched_4d_z_with_inverse(
     int outer_batch_size, int inner_batch_size, int N,
     doublecmplx *A_batch, 
@@ -487,7 +576,7 @@ int skpfa_batched_4d_d_with_inverse(
             return info;
         }
 
-        if (abs(pfaffians[idx]) > 1e-12) {
+        if (fabs(pfaffians[idx]) > 1e-12) {
             // Transpose matrix again for inverse computation
             transpose_matrix(current_matrix, work_matrix, N);
 
@@ -520,6 +609,234 @@ int skpfa_batched_4d_d_with_inverse(
     }
 
     // Cleanup
+    free(work);
+    free(initial_buffer);
+
+    return 0;
+}
+
+int skpfa_batched_4d_z_with_inverse_f(
+    int outer_batch_size, int inner_batch_size, int N,
+    doublecmplx *A_batch,
+    doublecmplx *pfaffians,
+    doublecmplx *inverses,
+    const char *UPLO, const char *MTHD)
+{
+    char uplo = toupper(UPLO[0]);
+    char mthd = toupper(MTHD[0]);
+
+    if (N < 0) return -1;
+    if (!A_batch || !pfaffians || !inverses) return -2;
+    if (uplo != 'U' && uplo != 'L') return -3;
+    if (mthd != 'P' && mthd != 'H') return -4;
+
+    if (N == 0) {
+        size_t total_matrices = (size_t)outer_batch_size * inner_batch_size;
+        for (size_t i = 0; i < total_matrices; i++) {
+            pfaffians[i] = doublecmplx_one;
+        }
+        return 0;
+    }
+
+    const size_t total_matrices = (size_t)outer_batch_size * inner_batch_size;
+    const size_t matrix_size = (size_t)N * N;
+
+    size_t initial_buffer_size =
+        sizeof(doublecmplx) * matrix_size +
+        sizeof(int) * 2 * N +
+        sizeof(double) * (N - 1);
+
+    void *initial_buffer = NULL;
+    if (posix_memalign(&initial_buffer, 64, initial_buffer_size) != 0)
+        return -100;
+
+    doublecmplx *work_pfaffian = (doublecmplx *)initial_buffer;
+    int *ipiv = (int *)(work_pfaffian + matrix_size);
+    int *iwork = ipiv + N;
+    double *rwork = (double *)(iwork + N);
+
+    int lwork_pf = -1;
+    int lwork_inv = -1;
+    doublecmplx qwork_pf = 0.0;
+    doublecmplx qwork_inv = 0.0;
+    int info = 0;
+    int ldim = N;
+
+    PFAPACK_zskpfa(&uplo, &mthd, &N, A_batch, &ldim, &qwork_pf,
+                   iwork, &qwork_pf, &lwork_pf, rwork, &info);
+    if (info != 0) {
+        free(initial_buffer);
+        return info;
+    }
+
+    zgetri_(&N, A_batch, &ldim, ipiv, &qwork_inv, &lwork_inv, &info);
+    if (info != 0) {
+        free(initial_buffer);
+        return info;
+    }
+
+    int lwork = (int)(creal(qwork_pf) > creal(qwork_inv) ? creal(qwork_pf) : creal(qwork_inv));
+
+    doublecmplx *work = NULL;
+    if (posix_memalign((void **)&work, 64, sizeof(doublecmplx) * lwork) != 0) {
+        free(initial_buffer);
+        return -105;
+    }
+
+    for (size_t idx = 0; idx < total_matrices; idx++) {
+        doublecmplx *current_matrix = A_batch + idx * matrix_size;
+        doublecmplx *current_inverse = inverses + idx * matrix_size;
+
+        memcpy(work_pfaffian, current_matrix, matrix_size * sizeof(doublecmplx));
+
+        PFAPACK_zskpfa(&uplo, &mthd, &N, work_pfaffian, &ldim, &pfaffians[idx],
+                        iwork, work, &lwork, rwork, &info);
+        if (info != 0) {
+            free(work);
+            free(initial_buffer);
+            return info;
+        }
+
+        if (cabs(pfaffians[idx]) > 1e-12) {
+            memcpy(current_inverse, current_matrix, matrix_size * sizeof(doublecmplx));
+
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    current_inverse[j + i * N] = -current_inverse[i + j * N];
+                }
+            }
+
+            zgetrf_(&N, &N, current_inverse, &ldim, ipiv, &info);
+            if (info != 0) {
+                free(work);
+                free(initial_buffer);
+                return info;
+            }
+
+            zgetri_(&N, current_inverse, &ldim, ipiv, work, &lwork, &info);
+            if (info != 0) {
+                free(work);
+                free(initial_buffer);
+                return info;
+            }
+        } else {
+            memset(current_inverse, 0, matrix_size * sizeof(doublecmplx));
+        }
+    }
+
+    free(work);
+    free(initial_buffer);
+
+    return 0;
+}
+
+int skpfa_batched_4d_d_with_inverse_f(
+    int outer_batch_size, int inner_batch_size, int N,
+    double *A_batch,
+    double *pfaffians,
+    double *inverses,
+    const char *UPLO, const char *MTHD)
+{
+    char uplo = toupper(UPLO[0]);
+    char mthd = toupper(MTHD[0]);
+
+    if (N < 0) return -1;
+    if (!A_batch || !pfaffians || !inverses) return -2;
+    if (uplo != 'U' && uplo != 'L') return -3;
+    if (mthd != 'P' && mthd != 'H') return -4;
+
+    if (N == 0) {
+        size_t total_matrices = (size_t)outer_batch_size * inner_batch_size;
+        for (size_t i = 0; i < total_matrices; i++) {
+            pfaffians[i] = 1.0;
+        }
+        return 0;
+    }
+
+    const size_t total_matrices = (size_t)outer_batch_size * inner_batch_size;
+    const size_t matrix_size = (size_t)N * N;
+
+    size_t initial_buffer_size =
+        sizeof(double) * matrix_size +
+        sizeof(int) * 2 * N;
+
+    void *initial_buffer = NULL;
+    if (posix_memalign(&initial_buffer, 64, initial_buffer_size) != 0)
+        return -100;
+
+    double *work_pfaffian = (double *)initial_buffer;
+    int *ipiv = (int *)(work_pfaffian + matrix_size);
+    int *iwork = ipiv + N;
+
+    int lwork_pf = -1;
+    int lwork_inv = -1;
+    double qwork_pf = 0.0;
+    double qwork_inv = 0.0;
+    int info = 0;
+    int ldim = N;
+
+    PFAPACK_dskpfa(&uplo, &mthd, &N, A_batch, &ldim, &qwork_pf,
+                   iwork, &qwork_pf, &lwork_pf, &info);
+    if (info != 0) {
+        free(initial_buffer);
+        return info;
+    }
+
+    dgetri_(&N, A_batch, &ldim, ipiv, &qwork_inv, &lwork_inv, &info);
+    if (info != 0) {
+        free(initial_buffer);
+        return info;
+    }
+
+    int lwork = (int)(qwork_pf > qwork_inv ? qwork_pf : qwork_inv);
+
+    double *work = NULL;
+    if (posix_memalign((void **)&work, 64, sizeof(double) * lwork) != 0) {
+        free(initial_buffer);
+        return -105;
+    }
+
+    for (size_t idx = 0; idx < total_matrices; idx++) {
+        double *current_matrix = A_batch + idx * matrix_size;
+        double *current_inverse = inverses + idx * matrix_size;
+
+        memcpy(work_pfaffian, current_matrix, matrix_size * sizeof(double));
+
+        PFAPACK_dskpfa(&uplo, &mthd, &N, work_pfaffian, &ldim, &pfaffians[idx],
+                       iwork, work, &lwork, &info);
+        if (info != 0) {
+            free(work);
+            free(initial_buffer);
+            return info;
+        }
+
+        if (fabs(pfaffians[idx]) > 1e-12) {
+            memcpy(current_inverse, current_matrix, matrix_size * sizeof(double));
+
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    current_inverse[j + i * N] = -current_inverse[i + j * N];
+                }
+            }
+
+            dgetrf_(&N, &N, current_inverse, &ldim, ipiv, &info);
+            if (info != 0) {
+                free(work);
+                free(initial_buffer);
+                return info;
+            }
+
+            dgetri_(&N, current_inverse, &ldim, ipiv, work, &lwork, &info);
+            if (info != 0) {
+                free(work);
+                free(initial_buffer);
+                return info;
+            }
+        } else {
+            memset(current_inverse, 0, matrix_size * sizeof(double));
+        }
+    }
+
     free(work);
     free(initial_buffer);
 
