29f8c2c1ba1855d5d380d7bd60d4c1b19daa2272
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 21:36:20 +0000

    add PFAPACK Performance (ubuntu-latest) (customSmallerIsBetter) benchmark result for 6016ef2b688174a39e212b44382d5439e733c42d

--- dev/bench/ubuntu-latest/data.js
+++ dev/bench/ubuntu-latest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769030393343,
+  "lastUpdate": 1769031380294,
   "repoUrl": "https://github.com/jjgoings/pfapack",
   "entries": {
     "PFAPACK Performance (ubuntu-latest)": [
@@ -64,6 +64,72 @@ window.BENCHMARK_DATA = {
             "name": "phase2_many_small",
             "value": 0.019041141199994626,
             "range": "± 0.000032",
+            "unit": "s",
+            "extra": "outer=8192 n=8"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "3915169+jjgoings@users.noreply.github.com",
+            "name": "Joshua Goings",
+            "username": "jjgoings"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6016ef2b688174a39e212b44382d5439e733c42d",
+          "message": "Merge pull request #6 from jjgoings/faster-batching\n\nOptimize batched Pfaffian computation",
+          "timestamp": "2026-01-21T13:33:49-08:00",
+          "tree_id": "cafc0beda999845a08646a08940e4dabd5e8d225",
+          "url": "https://github.com/jjgoings/pfapack/commit/6016ef2b688174a39e212b44382d5439e733c42d"
+        },
+        "date": 1769031380007,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "python_loop",
+            "value": 0.003708165248000796,
+            "range": "± 0.000045",
+            "unit": "s",
+            "extra": "min=0.003643 max=0.003926"
+          },
+          {
+            "name": "c_loop",
+            "value": 0.0006382905860829082,
+            "range": "± 0.000368",
+            "unit": "s",
+            "extra": "min=0.000576 max=0.012572"
+          },
+          {
+            "name": "batched_3d",
+            "value": 0.00008229020545592125,
+            "range": "± 0.000016",
+            "unit": "s",
+            "extra": "min=0.000076 max=0.000195"
+          },
+          {
+            "name": "batched_4d",
+            "value": 0.00002230612484733056,
+            "range": "± 0.000007",
+            "unit": "s",
+            "extra": "min=0.000021 max=0.000158"
+          },
+          {
+            "name": "phase2_tomography",
+            "value": 0.009788729200010948,
+            "range": "± 0.000027",
+            "unit": "s",
+            "extra": "n_sel=16"
+          },
+          {
+            "name": "phase2_many_small",
+            "value": 0.015645005399994714,
+            "range": "± 0.000486",
             "unit": "s",
             "extra": "outer=8192 n=8"
           }
